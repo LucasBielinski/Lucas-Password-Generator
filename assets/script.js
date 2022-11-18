62a5e14fee0d36f9d4013781a0583bdca7fc066a
LucasBielinski <115428815+LucasBielinski@users.noreply.github.com>
2022-11-18 15:11:18 -0500

    add template for java

--- assets/script.js
+++ assets/script.js
@@ -2,105 +2,24 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
-  var specialCharacters = [
-    "!",
-    '"',
-    "#",
-    "$",
-    "%",
-    "&",
-    "'",
-    "(",
-    ")",
-    "*",
-    "+",
-    ",",
-    "-",
-    ".",
-    "/",
-    ";",
-    ":",
-    "<",
-    ">",
-    "=",
-    "?",
-    "@",
-    "[",
-    "]",
-    "^",
-    "_",
-    "`",
-    "{",
-    "/",
-    "}",
-    "~",
-  ];
-  var uppercaseLetters = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
-  var lowercaseLetter = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-
-  var userInput = prompt(
-    "How many character would you like your password to be. Minimum 8, Maximum 128",
-    ""
-  );
+  var specialCharacters = [ "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ";", ":", "<", ">", "=", "?", "@", "[", "]", "^", "_", "`", "{", "/", "}", "~"];
+  var uppercaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var lowercaseLetter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
+  var numbers = [0,1,2,3,4,5,6,7,8,9];
+  var userInput = prompt("How many characters would you like your password to be. Minimum 8, Maximum 128","");
   if (!userInput);
   {
     return;
   }
+  userInput = number();
+
+var useSpecialCharacters = confirm("would you like to use special characters?");
+var useUppercaseLetter = confirm("would you like to use uppercase letters?");
+var useLowercaseLetter = confirm("would you like to use lower case letters?")
+var useNumbers = confirm("would you like to use numbers?")
 }
+ if useLowercaseLetter
+ ()
 
 // Write password to the #password input
 function writePassword() {
